4da05e61912f8cd84df58960003f7d3c6c0ca661
javadbat <javadbat@gmail.com>
2025-02-01 15:37:03 +0330

    feat: add build config

---
+++ build-config.ts
@@ -0,0 +1,22 @@
+import type { ReactComponentBuildConfig, WebComponentBuildConfig } from "../../tasks/build/builder/src/types.ts";
+
+export const webComponentList: WebComponentBuildConfig[] = [
+  {
+    name: "jb-modal",
+    path: "./lib/JBModal.ts",
+    outputPath: "./dist/JBModal.js",
+    umdName: "JBModal",
+  },
+];
+export const reactComponentList: ReactComponentBuildConfig[] = [
+  {
+    name: "jb-modal-react",
+    path: "./react/lib/JBModal.tsx",
+    outputPath: "./react/dist/JBModal.js",
+    external: ["jb-modal", "prop-types", "react"],
+    globals: {
+      react: "React",
+      "prop-types": "PropTypes",
+    },
+  },
+];
