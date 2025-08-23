6f472d8031658a9cbd14acedeba76648b546ab9a
javadbat <javadbat@gmail.com>
2025-08-23 21:27:16 +0330

    add mobile view

--- stories/JBModal.stories.tsx
+++ stories/JBModal.stories.tsx
@@ -47,32 +47,44 @@ export const ActionTest: Story = {
   }
 };
 
+export const MobileView: Story = {
+  args: {
+    isOpen:true,
+    children: <div className='modal-test-content'>Mobile View</div>
+  },
+  parameters: {
+    viewport: {
+      defaultViewport: 'mobile2', // Set default to mobile viewport
+    },
+  }
+
+}
 export const CloseDetail: Story = {
   render: (story) => {
     const [isOpen, setIsOpen] = useState(false);
     const onModalClose = (e: JBModalEventType<CustomEvent<any>>) => {
-      console.log("modal close event type:",e.detail.eventType);
+      console.log("modal close event type:", e.detail.eventType);
       setIsOpen(false);
     }
     return (
-        <div className='button-wrapper'>
-          <JBButton color='light' onClick={(e) => setIsOpen(true)}>Open Modal</JBButton>
-          <JBModal isOpen={isOpen} onClose={onModalClose} onUrlOpen={()=>setIsOpen(true)} id="MyModal">
-            <div className='modal-test-content' style={{ flexDirection: 'column', alignItems:'flex-start' }}>
-              <p>
-                `onClose` event of the modal called for 2 reason:
+      <div className='button-wrapper'>
+        <JBButton color='light' onClick={(e) => setIsOpen(true)}>Open Modal</JBButton>
+        <JBModal isOpen={isOpen} onClose={onModalClose} onUrlOpen={() => setIsOpen(true)} id="MyModal">
+          <div className='modal-test-content' style={{ flexDirection: 'column', alignItems: 'flex-start' }}>
+            <p>
+              `onClose` event of the modal called for 2 reason:
               <ul>
                 <li>when user click on modal background</li>
                 <li>when user hit back button in android or back button of the browser</li>
               </ul>
-              </p>
-              <p>to simulate the first one you just have to click on modal background and you can see developer tools console that shows `BACKGROUND_CLICK`</p>
-              <p>to experience the second scenario with back button since we are in storybook and storybook load stories in a `iframe` tag you should open story in <a target='_blank' href='/iframe.html?globals=&id=components-jbmodal--close-detail&viewMode=story'>isolated mode</a> then hit back button</p>
-              <q>back button scenario only works if your modal has an `id` attribute</q>
-              <JBButton onClick={() => { setIsOpen(false); }}>Close Modal</JBButton>
-            </div>
-          </JBModal>
-        </div>
+            </p>
+            <p>to simulate the first one you just have to click on modal background and you can see developer tools console that shows `BACKGROUND_CLICK`</p>
+            <p>to experience the second scenario with back button since we are in storybook and storybook load stories in a `iframe` tag you should open story in <a target='_blank' href='/iframe.html?globals=&id=components-jbmodal--close-detail&viewMode=story'>isolated mode</a> then hit back button</p>
+            <q>back button scenario only works if your modal has an `id` attribute</q>
+            <JBButton onClick={() => { setIsOpen(false); }}>Close Modal</JBButton>
+          </div>
+        </JBModal>
+      </div>
     )
   }
 };
