c0eaa455cc4d6bc045d4be883a02ea34944dc993
javadbat <javadbat@gmail.com>
2026-05-07 23:46:00 +0330

    update config

--- lib/global.d.ts
+++ lib/global.d.ts
@@ -5,10 +5,6 @@ declare module '*.css' {
     const value: FileStringModules;
     export default value;
 }
-declare module '*.html' {
-    const value: FileStringModules;
-    export default value.default;
-}
 declare module '*.svg' {
     const value: string;
     export default value;
